ac53660c7898d53b7359a076db3ff2541117afc6
Mehdi Druon <mehdidruon@protonmail.com>
2018-07-11 17:15:49 +0200

    Add AddQuotes component & use him in App.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
     <div class="container">
+        <app-add-quotes @quoteAdded='addQuotes'></app-add-quotes>
         <app-quotes-grid :quotes='quotes'></app-quotes-grid>
     </div>
 </template>
 
 <script>
     import QuotesGrid from './components/QuotesGrid.vue'
+    import AddQuotes from './components/AddQuotes.vue'
     export default {
       data: function() {
           return {
@@ -15,8 +17,14 @@
               maxQuote: 20
           }
       },
+      methods: {
+          addQuotes(quote) {
+              this.quotes.push(quote)
+          }
+      },
       components: {
-          appQuotesGrid: QuotesGrid
+          appQuotesGrid: QuotesGrid,
+          appAddQuotes: AddQuotes
       }
     }
 </script>

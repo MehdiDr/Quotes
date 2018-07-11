c3fa48847cfdd248fc83a87d15fd5f1dce511de0
Mehdi Druon <mehdidruon@protonmail.com>
2018-07-11 17:43:25 +0200

    Add a progress bar.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="container">
+        <app-header :quoteCount='quotes.length' :maxQuotes='maxQuotes'></app-header>
         <app-add-quotes @quoteAdded='addQuotes'></app-add-quotes>
         <app-quotes-grid :quotes='quotes' @quoteDeleted='deleteQuote'></app-quotes-grid>
         <div class='row'>
@@ -13,18 +14,20 @@
 <script>
     import QuotesGrid from './components/QuotesGrid.vue'
     import AddQuotes from './components/AddQuotes.vue'
+    import Header from './components/Header.vue'
     export default {
       data: function() {
           return {
               quotes: [
                   'Just a test quote'
               ],
-              maxQuote: 20
+              maxQuotes: 20
           }
       },
       methods: {
           addQuotes(quote) {
-              this.quotes.push(quote)
+              this.quotes.length >= this.maxQuotes ? alert('Please delete quote before add new') : '';
+              this.quotes.push(quote);
           },
           deleteQuote(i) {
               this.quotes.splice(i, 1);
@@ -32,7 +35,8 @@
       },
       components: {
           appQuotesGrid: QuotesGrid,
-          appAddQuotes: AddQuotes
+          appAddQuotes: AddQuotes,
+          appHeader: Header
       }
     }
 </script>

f0c3bbcbade5ca0dbdd9a1c130294d32692d636b
Mehdi Druon <mehdidruon@protonmail.com>
2018-07-11 16:53:40 +0200

    Update App to display the testing quote

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,23 @@
 <template>
     <div class="container">
-    
+        <app-quotes-grid :quotes='quotes'></app-quotes-grid>
     </div>
 </template>
 
 <script>
+    import QuotesGrid from './components/QuotesGrid.vue'
     export default {
-        
+      data: function() {
+          return {
+              quotes: [
+                  'Just a test quote'
+              ],
+              maxQuote: 20
+          }
+      },
+      components: {
+          appQuotesGrid: QuotesGrid
+      }
     }
 </script>
 

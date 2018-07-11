f0418e55368a380c11ab8c79cf929bf4ee717221
Mehdi Druon <mehdidruon@protonmail.com>
2018-07-11 16:53:00 +0200

    Add Quotes & QuotesGrid components

---
+++ src/components/QuotesGrid.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class='row'>
+    <app-quotes v-for='quote in quotes'>{{ quote }}</app-quotes>
+  </div>
+</template>
+
+<script>
+  import Quotes from './Quotes.vue'
+  export default {
+    props: ['quotes'],
+    components: {
+      appQuotes: Quotes
+    }
+  }
+</script>
+
+<style>
+
+</style>
+
